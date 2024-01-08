1e3fccac299de8ec2683b7c140b1edef9a79c36e
yigitsilto <47320854+yigitsilto@users.noreply.github.com>
2024-01-09 00:01:27 +0300

    sms verification implemented

--- resources/views/layouts/sms.blade.php
+++ resources/views/layouts/sms.blade.php
@@ -18,7 +18,7 @@
 <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
     <div>
         <p>
-            Sms Doğrulaması
+            Doğrulama
         </p>
     </div>
 
