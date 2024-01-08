78dced5668e1a3141929330d75a51ee1c87eacd7
yigitsilto <47320854+yigitsilto@users.noreply.github.com>
2024-01-08 23:20:17 +0300

    sms verification implemented

--- resources/views/user/room-detail.blade.php
+++ resources/views/user/room-detail.blade.php
@@ -123,7 +123,7 @@
                                 <div class="row">
                                    @foreach($room->getMedia('room_images') as $image)
                                         <div class="col-3 mt-3 p-1">
-                                            <a href="{{$image->getUrl()}}" target="_blank" rel="noopener noreferrer">
+                                            <a href="#" target="_blank" rel="noopener noreferrer">
                                                 <img src="{{$image->getUrl()}}" style="width: 400px; height: 200px; object-fit: cover; border: 2px solid #ccc; border-radius: 8px;" class="img-fluid" alt="image">
                                             </a>
                                         </div>
@@ -137,38 +137,6 @@
                 </div>
             </div>
         </div>
-        <footer class="footer pt-3  ">
-            <div class="container-fluid">
-                <div class="row align-items-center justify-content-lg-between">
-                    <div class="col-lg-6 mb-lg-0 mb-4">
-                        <div class="copyright text-center text-sm text-muted text-lg-start">
-                            © <script>
-                                document.write(new Date().getFullYear())
-                            </script>,
-                            made with <i class="fa fa-heart"></i> by
-                            <a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">Creative Tim</a>
-                            for a better web.
-                        </div>
-                    </div>
-                    <div class="col-lg-6">
-                        <ul class="nav nav-footer justify-content-center justify-content-lg-end">
-                            <li class="nav-item">
-                                <a href="https://www.creative-tim.com" class="nav-link text-muted" target="_blank">Creative Tim</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="https://www.creative-tim.com/presentation" class="nav-link text-muted" target="_blank">About Us</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="https://www.creative-tim.com/blog" class="nav-link text-muted" target="_blank">Blog</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="https://www.creative-tim.com/license" class="nav-link pe-0 text-muted" target="_blank">License</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </footer>
     </div>
 
 @endsection
