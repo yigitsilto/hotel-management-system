f49cf4dbf27cccb3bfd284c6d16b42549644a6b6
yigitsilto <47320854+yigitsilto@users.noreply.github.com>
2024-01-23 17:33:24 +0300

    excel import

--- app/Http/Requests/admin/UserUpdateEquest.php
+++ app/Http/Requests/admin/UserUpdateEquest.php
@@ -11,7 +11,7 @@ class UserUpdateEquest extends FormRequest
      */
     public function authorize(): bool
     {
-        return auth()->user()->role == 'ADMIN';
+        return auth()->user()->role == 'ADMIN' || auth()->user()->role == 'WORKER';
     }
 
     /**
