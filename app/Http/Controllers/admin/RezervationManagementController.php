72241c9ec85d1d93cedf70c75ceef2e3c70dea8d
yigitsilto <47320854+yigitsilto@users.noreply.github.com>
2024-01-23 14:13:09 +0300

    excel import

--- app/Http/Controllers/admin/RezervationManagementController.php
+++ app/Http/Controllers/admin/RezervationManagementController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\admin;
 use App\Enums\ReservationStatusEnum;
 use App\Http\Controllers\Controller;
 use App\Jobs\SendOrderApprovedSmsJob;
+use App\Models\AuthorizedHotel;
 use App\Models\Hotel;
 use App\Models\Reservation;
 use App\Models\Room;
@@ -25,18 +26,41 @@ class RezervationManagementController extends Controller
 
     public function index(): View
     {
-        $reservations = Reservation::query()
-                                   ->with([
-                                              'room',
-                                              'room.hotel'
-                                          ])
-                                   ->orderBy('created_at', 'desc')
-                                   ->paginate(10);
 
+        if (auth()->user()->role == 'ADMIN') {
+
+            $reservations = Reservation::query()
+                                       ->with([
+                                                  'room',
+                                                  'room.hotel'
+                                              ])
+                                       ->orderBy('created_at', 'desc')
+                                       ->paginate(10);
+
+        } else {
+            $authroizedHotels = AuthorizedHotel::query()
+                                               ->where('user_id', auth()->id())
+                                               ->get();
+
+
+            $reservations = Reservation::query()
+                                       ->with([
+                                                  'room',
+                                                  'room.hotel' => function ($query) use ($authroizedHotels) {
+                                                      $query->whereIn('id', $authroizedHotels->pluck('hotel_id'));
+                                                  },
+                                              ])
+                                       ->whereHas('room.hotel', function ($query) use ($authroizedHotels) {
+                                           $query->whereIn('id', $authroizedHotels->pluck('hotel_id'));
+                                       })
+                                       ->orderBy('created_at', 'desc')
+                                       ->paginate(10);
+
+        }
         return view('admin.reservationManagement.index', compact('reservations'));
     }
 
-    public function show(Reservation $reservation): View
+    public function show(Reservation $reservation)
     {
 
         $room = Room::query()
@@ -44,11 +68,43 @@ class RezervationManagementController extends Controller
 
         $hotel = Hotel::query()
                       ->findOrFail($room->hotel_id);
+
+
+        if (auth()->user()->role != 'ADMIN'){
+            $authroizedHotels = AuthorizedHotel::query()
+                                               ->where('user_id', auth()->id())
+                                               ->get();
+
+            if (!$authroizedHotels->contains('hotel_id', $hotel->id)){
+                return redirect()
+                    ->back()
+                    ->with('error', 'Yetkisiz işlem.');
+            }
+        }
+
         return view('admin.reservationManagement.show', compact('reservation', 'room', 'hotel'));
     }
 
     public function update(Request $request, Reservation $reservation): \Illuminate\Http\RedirectResponse
     {
+
+
+        if (auth()->user()->role != 'ADMIN'){
+            $room = Room::query()
+                          ->findOrFail($reservation->room_id);
+
+            $authroizedHotels = AuthorizedHotel::query()
+                                               ->where('user_id', auth()->id())
+                                               ->get();
+
+            if (!$authroizedHotels->contains('hotel_id', $room->hotel->id)){
+                return redirect()
+                    ->route('reservation.index')
+                    ->with('error', 'Yetkisiz işlem.');
+            }
+        }
+
+
         $request->validate([
                                'reservation_status' => 'required',
                                'paid_amount' => 'required|numeric',
