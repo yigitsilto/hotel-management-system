2952ed06573a562baa5cb149961eb0ce473737ee
yigitsilto <47320854+yigitsilto@users.noreply.github.com>
2024-01-23 17:46:30 +0300

    excel import

--- app/Http/Controllers/admin/SettingsController.php
+++ app/Http/Controllers/admin/SettingsController.php
@@ -3,31 +3,44 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\admin\UserCreateRequest;
-use App\Http\Requests\admin\UserUpdateEquest;
 use App\Models\Setting;
-use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class SettingsController extends Controller
 {
-    public function index(): View
+    public function index()
     {
-        $settings = Setting::query()->orderBy('key', 'desc')->get();
+        if (auth()->user()->role != 'ADMIN') {
+            return redirect()
+                ->route('hotel-management.index')
+                ->with('error', 'Yetkisiz işlem.');
+        }
+
+        $settings = Setting::query()
+                           ->orderBy('key', 'desc')
+                           ->get();
         return view('admin.settings', compact('settings'));
     }
 
     public function update(Request $request)
     {
+
+        if (auth()->user()->role != 'ADMIN') {
+            return redirect()
+                ->route('hotel-management.index')
+                ->with('error', 'Yetkisiz işlem.');
+        }
+
+
         foreach ($request->except('_token') as $key => $value) {
             Setting::updateOrCreate(['key' => $key], ['value' => $value]);
         }
 
-        return redirect()->route('settings.index')->with('success', 'Ayarlar güncellendi!');
+        return redirect()
+            ->route('settings.index')
+            ->with('success', 'Ayarlar güncellendi!');
     }
-
-
 
 
 }
