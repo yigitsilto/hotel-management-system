7d3726ff7cfbe63bcdd60de2f408dee10adb26a2
yigitsilto <47320854+yigitsilto@users.noreply.github.com>
2024-02-27 21:53:48 +0300

    test

--- app/Services/BankTransferCheckService.php
+++ app/Services/BankTransferCheckService.php
@@ -33,6 +33,7 @@ class BankTransferCheckService
 
         try
         {
+            dd($client);
 // Burda en çok kullanılan metodu örnekledim, birde bağlı müşteri metodu var onuda göstereceğim
             $response=$client->EkstreSorgulama($requestParams);
         }
