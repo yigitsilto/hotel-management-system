099f47259242cd4c6bbdc4e9a5034bc9f7205921
yigitsilto <47320854+yigitsilto@users.noreply.github.com>
2024-03-06 19:53:10 +0300

    bf

--- app/Http/Controllers/admin/TransactionController.php
+++ app/Http/Controllers/admin/TransactionController.php
@@ -12,7 +12,7 @@ class TransactionController extends Controller
      */
     public function index()
     {
-        $details = TransactionDetail::query()->orderBy('created_at', 'desc')->paginate(20);
+        $details = TransactionDetail::query()->with('reservation')->whereHas('reservation')->orderBy('created_at', 'desc')->paginate(20);
         return view('admin.transactionDetail.index', compact('details'));
     }
 
