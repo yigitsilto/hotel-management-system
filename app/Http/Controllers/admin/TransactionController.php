543cd4ca2a1665fd5b9a0d096f186352ebf4e196
yigitsilto <47320854+yigitsilto@users.noreply.github.com>
2024-03-06 23:06:32 +0300

    bf

--- app/Http/Controllers/admin/TransactionController.php
+++ app/Http/Controllers/admin/TransactionController.php
@@ -16,33 +16,35 @@ class TransactionController extends Controller
         $details = TransactionDetail::query()->with('reservation')->whereHas('reservation')
             ->orderBy('created_at', 'desc');
 
-//        if ($request->has('statusKey')) {
-//            if ($request->statusKey != 'all'){
-//                $details->where('status', $request->statusKey);
-//            }
-//        }
+
+        $details = TransactionDetail::query()->with('reservation');
+
+        if ($request->has('statusKey') && $request->statusKey != 'all') {
+            $details->where('status', $request->statusKey);
+        }
 
         if ($request->has('searchKey')) {
             $searchKey = $request->input('searchKey');
 
+            $details->where(function ($query) use ($searchKey) {
+                $query->whereHas('reservation.user', function ($q) use ($searchKey) {
+                    $q->whereRaw('LOWER(name) like ?', ['%' . $searchKey . '%'])
+                        ->orWhereRaw('LOWER(identity_number) like ?', ['%' . $searchKey . '%'])
+                        ->orWhereRaw('LOWER(phone_number) like ?', ['%' . $searchKey . '%'])
+                        ->orWhereRaw('LOWER(bank_transfer_code) like ?', ['%' . $searchKey . '%']);
+                });
 
-            $details->whereHas('reservation.user', function ($query) use ($searchKey) {
-                $query->whereRaw('LOWER(name) like ?', ['%' . $searchKey . '%'])
-                    ->orWhereRaw('LOWER(identity_number) like ?', ['%' . $searchKey . '%'])
-                    ->orWhereRaw('LOWER(phone_number) like ?', ['%' . $searchKey . '%'])
-                    ->orWhereRaw('LOWER(bank_transfer_code) like ?', ['%' . $searchKey . '%']);
-            });
+                if (!is_numeric($searchKey)) {
+                    $searchKey = strtolower($searchKey);
+                }
 
-            if (!is_numeric($searchKey)) {
-                $searchKey = strtolower($request->input('searchKey'));
-            } else {
-                $details->orWhereHas('reservation', function ($q) use($searchKey) {
+                $query->orWhereHas('reservation', function ($q) use ($searchKey) {
                     $q->where('id', $searchKey);
                 });
-            }
-
+            });
         }
 
+        $details->orderBy('created_at', 'desc');
 
 
         $details = $details->paginate(12);
